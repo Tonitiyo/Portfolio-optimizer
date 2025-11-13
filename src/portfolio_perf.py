0dc6a8579ca9b5e17d220f7314cff64706cf0049
Antoine-DD <156335142+Antoine-DD@users.noreply.github.com>
2025-11-13 23:58:16 +0100

    Update portfolio_perf.py

--- src/portfolio_perf.py
+++ src/portfolio_perf.py
@@ -5,8 +5,10 @@ import optimization_function as optf
 
 tickers = ["PE500.PA", "PANX.PA", "RS2K.PA", "DCAM.PA", "PTPXH.PA"]
 
-def portfolio_return(tickers, weights):
-    return weights @ src.annual_returns(tickers)
+def portfolio_returns(tickers, weights):
+    rets = src.returns(tickers)
+    weights = np.asarray(weights, dtype=float)
+    return rets.dot(weights)
 
 def portfolio_mu(tickers, weights):
     return weights @ src.mu(tickers)
@@ -18,6 +20,7 @@ def sharpe_ratio(tickers, weights):
     return (portfolio_mu(tickers, weights) - src.get_risk_free().iloc[-1,0]) / portfolio_vol(tickers, weights)
 
 #Exemple with the mean variance portfolio
-"""
-print(sharpe_ratio(tickers, optf.min_variance_portfolio(tickers)))
-"""
+
+"""print(portfolio_returns(tickers, optf.min_variance_portfolio(tickers)).iloc["2001:"])"""
+print(portfolio_mu(tickers, optf.min_variance_portfolio(tickers)))
+print(src.returns(tickers).loc["2001:"])
