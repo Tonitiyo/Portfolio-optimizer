e2a4f0212d62012def781967701d0dd7e7b55f8b
Antoine-DD <156335142+Antoine-DD@users.noreply.github.com>
2025-11-19 11:54:12 +0100

    Update optimization_function.py

--- src/optimization_function.py
+++ src/optimization_function.py
@@ -74,7 +74,7 @@ def min_variance_portfolio(tickers, short=False, leverage=True, max_weight=1.0):
     # Verify convergence
     if not result.success:
         error_msg = result.get('message', 'Optimisation failed')
-        raise ValueError(f"L'optimisation a échoué: {error_msg}")
+        raise ValueError(f"The optimisation failed: {error_msg}")
 
     # Desciption for each parameter
     if short and leverage:
@@ -88,3 +88,122 @@ def min_variance_portfolio(tickers, short=False, leverage=True, max_weight=1.0):
     
     return weights
 
+
+def risk_parity(tickers, short=False, leverage=False, max_weight=1): 
+    
+    n = len(tickers)
+    initial_weights = np.repeat(1/n, n)
+    
+    cov_matrix = src.covariance_matrix(tickers)
+    
+    eigvals = np.linalg.eigvalsh(cov_matrix)
+    if np.any(eigvals < -1e-10):
+        raise ValueError("La matrice de covariance n'est pas semi-définie positive")
+    
+    def objective(weights, cov_matrix):
+        port_var = weights.T @ cov_matrix @ weights
+        port_vol = np.sqrt(port_var)
+        
+        # Avoid to divide by 0
+        if port_vol < 1e-10:
+            return 1e10
+        
+        # marginal contributions
+        mrc = cov_matrix @ weights / port_vol
+        
+        # risk contributions
+        rc = weights * mrc
+        
+        # target
+        target_rc = port_vol / n
+        
+        return np.sum((rc - target_rc)**2)
+    
+    # Constraints : sum = 1
+    constraints = [{'type': 'eq', 'fun': lambda w: np.sum(w) - 1}]
+    
+    # leverage constrains
+    if short and not leverage:
+        constraints.append({
+            'type': 'ineq',
+            'fun': lambda w: 1.0 - np.sum(np.abs(w))
+        })
+    
+    # Bornes
+    if short:
+        bounds = tuple([(-max_weight, max_weight) for _ in range(n)])
+    else:
+        bounds = tuple([(0, max_weight) for _ in range(n)])
+    
+    # Optimization
+    result = minimize(
+        fun=objective,
+        x0=initial_weights,
+        args=(cov_matrix,),
+        method='SLSQP',
+        constraints=constraints,
+        bounds=bounds,
+        options={'ftol': 1e-9, 'maxiter': 1000}
+    )
+    
+    if not result.success:
+        error_msg = result.get('message', 'Optimisation failed')
+        raise ValueError(f"L'optimisation a échoué: {error_msg}")
+    
+    return result.x
+
+print(risk_parity(tickers))
+
+#MSR 
+
+def msr(tickers, short=False, leverage=False, max_weights=1):
+    n = len(tickers)
+    initial_weights = np.repeat(1/n, n)
+
+    cov_matrix = src.covariance_matrix(tickers)
+
+    def objective(cov_matrix):
+
+        #Calculate parameters 
+        port_rets = weights @ src.mu(tickers)
+        port_var = weights.T @ cov_matrix @ weights
+        port_vol = np.sqrt(port_var)
+        rf = src.get_risk_free(tickers)
+
+        #Calculate excess return 
+        excess_rets = port_rets - rf
+        #return sharpe ratio 
+
+        return port_rets / port_vol
+
+    # Constraints : sum = 1
+    constraints = [{'type': 'eq', 'fun': lambda w: np.sum(w) - 1}]
+    
+    # leverage constrains
+    if short and not leverage:
+        constraints.append({
+            'type': 'ineq',
+            'fun': lambda w: 1.0 - np.sum(np.abs(w))
+        })
+    
+    # Bornes
+    if short:
+        bounds = tuple([(-max_weight, max_weight) for _ in range(n)])
+    else:
+        bounds = tuple([(0, max_weight) for _ in range(n)])
+
+    result = maximize(
+        fun = objective,
+        args = (cov_matrix,),
+        methods = 'SLSQP'
+        constraints = constraints,
+        bounds = bounds,
+        options={'ftol': 1e-9, 'maxiter': 1000}
+    )
+
+    return result.x
+            
+#Max decorrelation
+
+
+#Black Litterman
